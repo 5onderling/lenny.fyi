7a60474abc2671319b6af26248679161982c7572
5onderling <lenny.anders.98@gmail.com>
2020-02-20 00:04:10 +0100

    don't change navigation on hashchange

--- src/scripts/stickyNavigation.js
+++ src/scripts/stickyNavigation.js
@@ -3,11 +3,22 @@ export default ({ selector = '.nav', fixedClass = 'nav--fixed' } = {}) => {
   if (!nav) return;
 
   let prevY = window.pageYOffset;
-  window.addEventListener('scroll', e => {
+  const scrollHandler = () => {
     const curY = window.pageYOffset;
     if (curY === prevY || (curY && curY < window.innerHeight / 3)) return;
 
     nav.classList[curY && curY < prevY ? 'add' : 'remove'](fixedClass);
     prevY = curY;
+  };
+  window.addEventListener('scroll', scrollHandler);
+
+  // don't make the navigation sticky by navigating through anchors in history
+  window.addEventListener('hashchange', () => {
+    window.removeEventListener('scroll', scrollHandler);
+    window.addEventListener(
+      'scroll',
+      () => window.addEventListener('scroll', scrollHandler),
+      { once: true }
+    );
   });
 };
