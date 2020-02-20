2177df72bea4d574331ab999b7d5a1a0f76d456d
5onderling <lenny.anders.98@gmail.com>
2020-02-20 22:56:13 +0100

    Make listeners for cursor passive

--- src/scripts/stickyNavigation.js
+++ src/scripts/stickyNavigation.js
@@ -10,15 +10,15 @@ export default ({ selector = '.nav', fixedClass = 'nav--fixed' } = {}) => {
     nav.classList[curY && curY < prevY ? 'add' : 'remove'](fixedClass);
     prevY = curY;
   };
-  window.addEventListener('scroll', scrollHandler);
+  window.addEventListener('scroll', scrollHandler, { passive: true });
 
   // don't make the navigation sticky by navigating through anchors in history
   window.addEventListener('hashchange', () => {
-    window.removeEventListener('scroll', scrollHandler);
+    window.removeEventListener('scroll', scrollHandler, { passive: true });
     window.addEventListener(
       'scroll',
-      () => window.addEventListener('scroll', scrollHandler),
-      { once: true }
+      () => window.addEventListener('scroll', scrollHandler, { passive: true }),
+      { once: true, passive: true }
     );
   });
 };
