8071210487bd85bf4e8ff8214e3cc35000bbab77
5onderling <lenny.anders.98@gmail.com>
2020-03-12 20:25:31 +0100

    implement "routeHandler", add ":scope" polyfill

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,47 @@ import skipNavigation from './skipNavigation';
 import stickyNavigation from './stickyNavigation';
 import mobileNavigation from './mobileNavigation';
 import themeSwitcher from './themeSwitcher';
-import hotlinks from './hotlinks';
+import RouteHandler from './routeHandler';
 import postWebshare from './postWebshare';
 
 const main = () => {
-  const router = hotlinks({ elementSelector: 'main' });
+  // :scope polyfill
+  (function(elProto) {
+    try {
+      document.querySelector(':scope');
+    } catch (error) {
+      var scope = /:scope(?![\w-])/gi;
+      ['querySelector', 'querySelectorAll', 'matches', 'closest'].forEach(
+        function(entry) {
+          var orig = elProto[entry];
+          if (!orig) return;
+          elProto[entry] = function(query) {
+            if (!scope.test(query)) return orig.apply(this, arguments);
+
+            var attr = 'q' + Math.floor(Math.random() * 9000000) + 1000000;
+            this.setAttribute(attr, '');
+            arguments[0] = query.replace(scope, '[' + attr + ']');
+            var res = orig.apply(this, arguments);
+            this.removeAttribute(attr);
+
+            return res;
+          };
+        }
+      );
+    }
+  })(Element.prototype);
+
+  const router = new RouteHandler({
+    elementSelector: '.content',
+    on: {
+      beforePageSave() {
+        console.log('save');
+      },
+      afterPageUpdate() {
+        console.log('updated');
+      }
+    }
+  });
 
   cursor();
   themeSwitcher();
@@ -16,7 +52,7 @@ const main = () => {
   skipNavigation();
   postWebshare({ router });
 
-  router.init();
+  router.start();
 };
 
 // polyfill addEventlistener option { once: true }
