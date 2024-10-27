3ee7d9a45e1cdae37eca2af5c4639f2d19f74c34
Lenny Anders <hey@lenny.fyi>
2024-10-27 21:48:16 +0100

    show random quote on every build on homepage

--- src/index.11ty.js
+++ src/index.11ty.js
@@ -1,3 +1,4 @@
+import { quotes } from './index/quotes.js';
 import { getImageHtml } from './utils/getImage.js';
 import { md } from './utils/index.js';
 
@@ -7,64 +8,67 @@ export const data = {
   order: 1,
 };
 
-export const render = async () => md`
-  <div class="hero">
-    ${await getImageHtml('./src/assets/avatar.png', {
-      width: 640,
-      class: 'hero__image',
-      alt: 'My avatar (An image of Majime Mitsuya from The Great Passage)',
-      lazy: false,
-    })}
-    <h1 class="hero__headline">
-      <small>Hey I'm</small><br/>
-      Lenny Anders
-    </h1>
-  </div>
+export const render = async () => {
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+  return md`
+    <div class="hero">
+      ${await getImageHtml('./src/assets/avatar.png', {
+        width: 640,
+        class: 'hero__image',
+        alt: 'My avatar (An image of Majime Mitsuya from The Great Passage)',
+        lazy: false,
+      })}
+      <h1 class="hero__headline">
+        <small>Hey I'm</small><br/>
+        Lenny Anders
+      </h1>
+    </div>
 
-  ## About me
+    ## About me
 
-  I'm a frontend developer based in Kürten, Germany. I started learning developing in 2015 with basic HTML and CSS, fell in love with it since then , and never stopped learning.
+    I'm a frontend developer based in Kürten, Germany. I started learning developing in 2015 with basic HTML and CSS, fell in love with it since then , and never stopped learning.
 
-  Currently, I'm working as an angular frontend developer at Ströer.
+    Currently, I'm working as an angular frontend developer at Ströer.
 
-  In my free time, I'm learning a lot about accessibility and HTML markup in general. When I'm not programming, I probably watch a great piece of audiovisual entertainment, listen to fantastic music, browse through Reddit/Twitter and enjoy wonderful art. In May 2023 I started learning piano.
+    In my free time, I'm learning a lot about accessibility and HTML markup in general. When I'm not programming, I probably watch a great piece of audiovisual entertainment, listen to fantastic music, browse through Reddit/Twitter and enjoy wonderful art. In May 2023 I started learning piano.
 
-  ## My skills
+    ## My skills
 
-  Over the time I learned a few things, I mostly like writing vanilla JavaScript, TypeScript, CSS, SCSS, HTML5 and Voby. For bundling I like to use Vite or esbuild.
+    Over the time I learned a few things, I mostly like writing vanilla JavaScript, TypeScript, CSS, SCSS, HTML5 and Voby. For bundling I like to use Vite or esbuild.
 
-  _Here's a fancy list of things I can do / use (well):_
+    _Here's a fancy list of things I can do / use (well):_
 
-  - CSS / SASS / SCSS / PostCSS
-  - JavaScript / TypeScript
-  - Vite / esbuild / Rollup
-  - Vue / Angular / Voby / Preact
-  - Node.js / NPM
-  - .net / C#
+    - CSS / SASS / SCSS / PostCSS
+    - JavaScript / TypeScript
+    - Vite / esbuild / Rollup
+    - Vue / Angular / Voby / Preact
+    - Node.js / NPM
+    - .net / C#
 
-  ## Here you can find me
+    ## Here you can find me
 
-  - [<span class="icon icon--twitter"></span> Twitter / x](https://twitter.com/5onderling)
-  - [<span class="icon icon--letterboxd"></span> Letterboxd](https://letterboxd.com/5onderling)
-  - [<span class="icon icon--anilist"></span> AniList](https://anilist.co/user/lenny)
-  - [<span class="icon icon--discogs"></span> Discogs](https://www.discogs.com/user/5onderling)
-  - [<span class="icon icon--linkedin"></span> Linkedin](https://www.linkedin.com/in/5onderling)
-  - [<span class="icon icon--github"></span> Github](https://github.com/5onderling)
-  - [<span class="icon icon--email"></span> E-Mail](mailto:hey@lenny.fyi)
+    - [<span class="icon icon--twitter"></span> Twitter / x](https://twitter.com/5onderling)
+    - [<span class="icon icon--letterboxd"></span> Letterboxd](https://letterboxd.com/5onderling)
+    - [<span class="icon icon--anilist"></span> AniList](https://anilist.co/user/lenny)
+    - [<span class="icon icon--discogs"></span> Discogs](https://www.discogs.com/user/5onderling)
+    - [<span class="icon icon--linkedin"></span> Linkedin](https://www.linkedin.com/in/5onderling)
+    - [<span class="icon icon--github"></span> Github](https://github.com/5onderling)
+    - [<span class="icon icon--email"></span> E-Mail](mailto:hey@lenny.fyi)
 
-  ## Things I use
+    ## Things I use
 
-  - **Editor:** [Visual Studio Code](https://code.visualstudio.com/)
-  - **Programming font:** [Fira Code](https://github.com/tonsky/FiraCode)
-  - **Programming theme:** [SynthWave '84](https://github.com/robb0wen/synthwave-vscode)
-  - **Computer:** MacBook Pro (M3 Max, 36 GB RAM)
-  - **Monitor:** LG 27GP950 and Eizo FlexScan EV2450-BK (older model)
-  - **Keyboard:** Ducky One 2 mini
-  - **Mouse:** Logitech MX Master 3
+    - **Editor:** [Visual Studio Code](https://code.visualstudio.com/)
+    - **Programming font:** [Fira Code](https://github.com/tonsky/FiraCode)
+    - **Programming theme:** [SynthWave '84](https://github.com/robb0wen/synthwave-vscode)
+    - **Computer:** MacBook Pro (M3 Max, 36 GB RAM)
+    - **Monitor:** LG 27GP950 and Eizo FlexScan EV2450-BK (older model)
+    - **Keyboard:** Ducky One 2 mini
+    - **Mouse:** Logitech MX Master 3
 
-  ---
+    ---
 
-  > You know what kind of plan never fails? No plan. No plan at all. You know why? Because life cannot be planned. Look around you. Did you think these people made a plan to sleep in the sports hall with you? But here we are now, sleeping together on the floor. So, there's no need for a plan. You can't go wrong with no plans. We don't need to make a plan for anything. It doesn't matter what will happen next. Even if the country gets destroyed or sold out, nobody cares. Got it?
+    > ${quote.quote}
 
-  — [Ki-taek (Parasite)](https://www.imdb.com/title/tt6751668/quotes/qt4669228)
+    — [${quote.author}](${quote.link})
 `;
+};
---
+++ src/index/quotes.js
@@ -0,0 +1,38 @@
+/** @type {{quote: string; author: string; link: string }[]} */
+export const quotes = [
+  {
+    quote: `YEAH`,
+    author: 'John Wick (John Wick: Chapter 4)',
+    link: 'https://letterboxd.com/film/john-wick-chapter-4/',
+  },
+  {
+    quote: `In the amazing book Moby Dick by the author Herman Melville, the author recounts his story of being at sea. In the first part of his book, the author, calling himself Ishmael, is in a small sea-side town and he is sharing a bed with a man named Queequeg, and I felt saddest of all when I read the boring chapters that were only descriptions of whales, because I knew that the author was just trying to save us from his own sad story, just for a little while.`,
+    author: 'Ellie (The Whale)',
+    link: 'https://letterboxd.com/film/the-whale-2022/',
+  },
+  {
+    quote: `Why ever climb higher? Be the first? Why risk death? Why do something so futile? I know why now. There doesn't need to be a reason. For some, the mountains aren't a goal, but a path. And the summit, a step. Once there, all that's left is to keep going.`,
+    author: 'Narrator (The Summit of the Gods)',
+    link: 'https://letterboxd.com/film/the-summit-of-the-gods/',
+  },
+  {
+    quote: `I must not fear. Fear is the mind-killer. Fear is the little death that brings obliteration. I will face my fear and I will permit it to pass over me and through me. And when it has gone past... I will turn the inner eye to see its path. Where the fear has gone there will be nothing. Only I will remain.`,
+    author: 'Lady Jessica Atreides (Dune)',
+    link: 'https://letterboxd.com/film/dune-2021/',
+  },
+  {
+    quote: `It may be good, it may be not, but just take the adventure. Don't use me as an excuse not to live.`,
+    author: 'Connie (Marcel the Shell with Shoes On)',
+    link: 'https://letterboxd.com/film/marcel-the-shell-with-shoes-on-2021/',
+  },
+  {
+    quote: `You know what kind of plan never fails? No plan. No plan at all. You know why? Because life cannot be planned. Look around you. Did you think these people made a plan to sleep in the sports hall with you? But here we are now, sleeping together on the floor. So, there's no need for a plan. You can't go wrong with no plans. We don't need to make a plan for anything. It doesn't matter what will happen next. Even if the country gets destroyed or sold out, nobody cares. Got it?`,
+    author: 'Ki-taek (Parasite)',
+    link: 'https://letterboxd.com/film/parasite-2019/',
+  },
+  {
+    quote: `I've seen things you people wouldn't believe. Attack ships on fire off the shoulder of Orion. I watched C-beams glitter in the dark near the Tannhauser gate. All those moments will be lost in time... like tears in rain... Time to die.`,
+    author: 'Batty (Blade Runner)',
+    link: 'https://letterboxd.com/film/blade-runner/',
+  },
+];
