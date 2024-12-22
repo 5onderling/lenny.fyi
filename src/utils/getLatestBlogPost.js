8f3115beb9e6df4508571143c59e22aec4b9dd49
Lenny Anders <hey@lenny.fyi>
2024-12-22 13:09:28 +0100

    fix blogroll entries with html element like things in title

--- src/utils/getLatestBlogPost.js
+++ src/utils/getLatestBlogPost.js
@@ -11,14 +11,17 @@ const getFormattedDate = (date) => {
   });
 };
 
+/** @param {cheerio.Cheerio<Element>} element */
+const getHtmlSaveString = (element) => element.text().replaceAll('<', '&lt;');
+
 const getEntries = (siteTextContent) => {
   const $ = cheerio.load(siteTextContent, { xml: true });
   const atomEntries = $('feed[xmlns="http://www.w3.org/2005/Atom"] entry');
   if (atomEntries.length) {
     return atomEntries.toArray().map((entry) => {
-      const title = $(entry).find('title').text();
+      const title = getHtmlSaveString($(entry).find('title'));
       const link = $(entry).find('link')[0]?.attribs.href;
-      const updated = $(entry).find('updated').text();
+      const updated = getHtmlSaveString($(entry).find('updated'));
       return { title, link, updated: updated && new Date(updated) };
     });
   }
@@ -26,9 +29,9 @@ const getEntries = (siteTextContent) => {
   const rssEntries = $('rss item');
   if (rssEntries.length) {
     return rssEntries.toArray().map((entry) => {
-      const title = $(entry).find('title').text();
+      const title = getHtmlSaveString($(entry).find('title'));
       const link = $(entry).find('link').text();
-      const updated = $(entry).find('pubDate').text();
+      const updated = getHtmlSaveString($(entry).find('pubDate'));
       return { title, link, updated: updated && new Date(updated) };
     });
   }
